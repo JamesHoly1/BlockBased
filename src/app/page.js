e4967d58b357df189887f1c4401d149a627238eb
JamesHoly1 <jamesdholyoak@protonmail.com>
2025-06-03 13:48:22 +0200

    BlockBased Update

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,39 @@
 import Image from "next/image";
 import Header from "./components/header.js";
-import { Goldman } from 'next/font/google';
+import { Montserrat } from 'next/font/google';
+import { Open_Sans } from 'next/font/google';
 
-const goldman = Goldman({
+
+
+const montserrat = Montserrat({
   subsets: ['latin'],
-  weight: ['400', '700'],
+  weight: ['600'],
 });
+
 
 export default function Home() {
   return (
-    <div className={`grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen gap-16 p-10 bg-[url('/blockbg.png')] ${goldman.className}`}>
-      <Header />
-      <main className="flex flex-col gap-18 row-start-2 items-center">
-        <h2 className="text-7xl text-gray-200 w-2/3 text-center ">The central hub for everything crypto.</h2>
-        <ol className="list-inside list-decimal text-md/6 text-center">
-          <li className="mb-2 tracking-[-.01em] pl-4">
-            Follow @blockbased_ on X.
-          </li>
-          <li className="tracking-[-.01em] pl-4">
-            Turn notifs on and share.
-          </li>
-        </ol>
+    <div className={`grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen gap-16 p-2 sm:p-10 bg-[url('/blockbg.png')]`}>
+      <main className="flex flex-col gap-12 sm:gap-18 row-start-2 items-center">
+        <Image
+            className=""
+            src="/blockbasedicon.svg"
+            alt="BlockBased logo"
+            width={70}
+            height={70}
+            priority
+        />
+        <h2 className={`text-[46px] sm:text-[70px] text-gray-200 w-4/5 sm:w-2/3 text-center leading-[60px] sm:leading-[80px] ${montserrat.className}`}>The Central Hub for Everything Crypto</h2>
+        <div className="flex flex-col justify-center gap-4" >
+          <ol className="list-inside list-decimal text-md/6 text-center">
+            <li className={`text-lg mb-2 tracking-[-.01em] pl-4 ${montserrat.className}`}>
+              Follow @blockbased_ on X
+            </li>
+            <li className={`text-lg mb-2 tracking-[-.01em] pl-4 ${montserrat.className}`}>
+              Engage & spread the word
+            </li>
+          </ol>
+        </div>
       </main>
     </div>
   );
